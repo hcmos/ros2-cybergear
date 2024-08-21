563e18ed026dcb08b4e9995a3e784ad9d6867127
hcmos <c1000702@planet.kanazawa-it.ac.jp>
2024-08-21 16:19:10 +0900

    🐛fix:main.cpp

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char * argv[]){
     auto socketcan_cybergear_node = std::make_shared<socketcan_interface::SocketcanInterface>("cybergear", nodes_option);
     auto cybergear_interface_node = std::make_shared<cybergear_interface::CybergearInterface>(nodes_option);
 
-    // exec.add_node(socketcan_cybergear_node);
+    exec.add_node(socketcan_cybergear_node);
     exec.add_node(cybergear_interface_node);
 
     exec.spin();
