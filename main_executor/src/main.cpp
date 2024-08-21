7089fff3af63a9bd2b89544365c770b5e5d4b6ef
hcmos <c1000702@planet.kanazawa-it.ac.jp>
2024-08-21 15:48:03 +0900

    ✨feat:first ci

---
+++ main_executor/src/main.cpp
@@ -0,0 +1,23 @@
+#include <rclcpp/rclcpp.hpp>
+
+#include "socketcan_interface/socketcan_interface_node.hpp"
+#include "cybergear_interface/cybergear_interface_node.hpp"
+
+int main(int argc, char * argv[]){
+    rclcpp::init(argc,argv);
+    rclcpp::executors::MultiThreadedExecutor exec;
+
+    rclcpp::NodeOptions nodes_option;
+    nodes_option.allow_undeclared_parameters(true);
+    nodes_option.automatically_declare_parameters_from_overrides(true);
+
+    auto socketcan_cybergear_node = std::make_shared<socketcan_interface::SocketcanInterface>("cybergear", nodes_option);
+    auto cybergear_interface_node = std::make_shared<cybergear_interface::CybergearInterface>(nodes_option);
+
+    // exec.add_node(socketcan_cybergear_node);
+    exec.add_node(cybergear_interface_node);
+
+    exec.spin();
+    rclcpp::shutdown();
+    return 0;
+}
